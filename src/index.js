910b9e9d4bd5b65e033d4c29cf0b5f0ea1cb0b17
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-02-09 16:42:58 +0900

    feat: Create simple web application

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 // index.js
 // This is the main entry point of our application
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => res.send('Hello World!!!'));
+
+app.listen(port, () =>
+    console.log('Server running at http://localhost:${port}')
+);
